0bd6bd8a0bade228d105531acdb4d9e98b2e96ad
杨江峰 <yjfn2141@corp.netease.com>
2016-07-30 15:33:13 +0800

    使用auto类型推导

--- cpptest2/vectortest.cpp
+++ cpptest2/vectortest.cpp
@@ -7,11 +7,21 @@ void print_vec(vector<int> a){
 	for (vector<int>::iterator iter = a.begin(); iter != a.end(); iter++){
 		cout << *iter << "  ";
 	}
+	for (auto item : a){
+		cout << item;
+	}
+
 	cout << endl;
 }
 
 int main(){
 	cout << 123 << endl;
+
+	int xx[5] = { 1, 2, 3, 4, 5 };
+	for (int item : xx){
+		cout << item;
+	}
+
 	vector<int> vec;
 	int len = 10;
 	for (int i = 0; i <= len ; i++){
@@ -24,6 +34,9 @@ int main(){
 	{
 		newvec.push_back(i+100);
 	}
+
+
+
 	vec.insert(vec.begin(),newvec.begin(),newvec.end());
 	print_vec(vec);
 	cin.get();
