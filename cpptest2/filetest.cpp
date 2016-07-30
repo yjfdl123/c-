97758bd4214ea84b2f316cecebd8231996d5973c
杨江峰 <yjfn2141@corp.netease.com>
2016-07-30 13:15:42 +0800

    file test

--- cpptest2/filetest.cpp
+++ cpptest2/filetest.cpp
@@ -1,9 +1,26 @@
 # include <stdio.h>
 # include <iostream>
+# include <fstream>
+# include <cstdlib>
 using namespace std;
 
 int main(){
 	cout << 123 << endl;
+	ifstream fin;
+	ofstream fout;
+	fout.open("out.txt");
+	int len = 100;
+	int i;
+	for  (i = 0; i < len ; i ++ ){
+		fout << "123"<<i  << endl;
+	}
+	string filename = string("3.txt");
+	fin.open(filename);
+	if (!fin.is_open()){
+		std::cout << filename.c_str() << "open filed" << endl;
+		cerr << "can't open file" << endl;
+	}
+	
 	cin.get();
 	return 0;
 }
